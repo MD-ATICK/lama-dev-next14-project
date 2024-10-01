cd16534aca40ac4738311ea66188307f93daab90
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-10-01 17:45:43 +0600

    seo add

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 
+import { auth } from '@/lib/auth'
 import logo from '@/public/favicon.ico'
 import Image from 'next/image'
 import Links from './Links'
-import { auth } from '@/lib/auth'
 export default async function Navbar() {
 
   const session = await auth()
@@ -11,7 +11,7 @@ export default async function Navbar() {
     <div className=" sticky top-0  z-20 left-0 border-b border-gray-900 mx-auto h-20 flex bg-[#000016] justify-between items-center container">
       <div className=' items-center gap-2 flex'>
         <Image src={logo} alt='logo' width={35} height={35} />
-        <h1 className=' font-bold text-xl'>Logo.</h1>
+        <h1 className=' font-bold text-xl'>LuxoraStore</h1>
       </div>
       <Links session={session} />
     </div>
